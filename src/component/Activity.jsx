230598b8f1f2026b400ee5d8ff6711e6fa76bf4c
wulandaridewi69 <dewi.wulandari.ruslan@gmail.com>
2022-09-04 23:27:17 +0700

    web end

--- src/component/Activity.jsx
+++ src/component/Activity.jsx
@@ -4,17 +4,17 @@ import Layout from './Layout';
 const Activity = () => {
 
     return (
-        <div className='border mt-20'>
+        <div className='border mt-20 mb-96'>
       <h5>Activitas</h5>
-      <div>
+      <div className='pb-4 pt-4'>
           <p>Yusron Baru saja menambahkan lokasi baru Kantor Cabang Jagakarsa</p>
           <p>Hari ini, 06.00 WIB</p>
       </div>
-      <div>
+      <div className='pb-4'>
           <p>Bintang baru saja menghapus sublokasi KCP Tebet 4 dari induk Kantor Cabang Tebet</p>
           <p>Kemarin, 17.32 WIB</p>
       </div>
-      <div>
+      <div className='pb-4'>
           <p>Yusron melakukan perubahan profil pada Induk Kantor Cabang Bekasi</p>
           <p>Kemarin, 17.32 WIB</p>
       </div>
